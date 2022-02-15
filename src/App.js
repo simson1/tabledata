f0e93b33e8c99cd02b41440c6a29eb3862f47876
simson1 <jaydave422@gmail.com>
2022-02-15 11:17:18 +0530

    Finished task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
-import logo from "./logo.svg";
-import "./App.css";
+import * as React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import MaterialTable from "./Table";
+import { connect } from "react-redux";
+import {
+  fetchData,
+  dataUpdate,
+  setEditIndex,
+} from "./actions/tableDataActions";
+function App({ dispatch, loading, data, error, editIdx }) {
+  React.useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
 
-function App() {
-  return <div className="App"></div>;
+  const handleRemove = (i) => {
+    dispatch(dataUpdate(data.filter((row, j) => j !== i)));
+  };
+
+  const startEditing = (i) => {
+    dispatch(setEditIndex(i));
+  };
+
+  const handleChange = (e, name, i) => {
+    const { value } = e.target;
+    dispatch(
+      dataUpdate(
+        data.map((row, j) => (j === i ? { ...row, [name]: value } : row))
+      )
+    );
+  };
+
+  const stopEditing = () => {
+    dispatch(setEditIndex(-1));
+  };
+
+  return (
+    <MuiThemeProvider>
+      <div className="App">
+        <MaterialTable
+          data={data}
+          header={[
+            {
+              displayName: "First name",
+              propName: "first_name",
+            },
+            {
+              displayName: "Last name",
+              propName: "last_name",
+            },
+            {
+              displayName: "Avatar",
+              propName: "avatar",
+            },
+            {
+              displayName: "Email",
+              propName: "email",
+            },
+          ]}
+          handleRemove={handleRemove}
+          startEditing={startEditing}
+          editIdx={editIdx}
+          handleChange={handleChange}
+          stopEditing={stopEditing}
+        />
+      </div>
+    </MuiThemeProvider>
+  );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  data: state.tableData,
+  loading: state.loading,
+  error: state.error,
+  editIdx: state.editIdx,
+});
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import tableDataReducer from './reducers/tableDataReducer'
+
+const store = createStore(
+  tableDataReducer,
+  applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
